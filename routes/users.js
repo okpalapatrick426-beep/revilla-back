c4cbc4885f5731921ee192acf623b832f57f9eca
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-13 04:06:37 +0100

    Add route to get all users

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, updateProfile, updateLocationSharing, searchUsers } = require('../controllers/userController');
+const { getProfile, updateProfile, updateLocationSharing, searchUsers, getAllUsers } = require('../controllers/userController');
 const { auth } = require('../middleware/auth');
 
+router.get('/all', auth, getAllUsers);
 router.get('/search', auth, searchUsers);
 router.get('/:id', auth, getProfile);
 router.put('/me', auth, updateProfile);
-// User explicitly opts in/out of location sharing
 router.put('/me/location', auth, updateLocationSharing);
 
 module.exports = router;
