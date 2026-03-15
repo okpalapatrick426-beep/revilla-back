63b9c2f8d8e2a0b0a52ec301eb9c6017f02b665c
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-15 12:40:02 +0100

    Refactor GroupMember model attributes
    Changed role type from ENUM to STRING and added timestamps.

--- models/GroupMember.js
+++ models/GroupMember.js
@@ -3,8 +3,5 @@ module.exports = (sequelize) => sequelize.define('GroupMember', {
   id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   groupId: { type: DataTypes.UUID, allowNull: false },
   userId: { type: DataTypes.UUID, allowNull: false },
-  role: { type: DataTypes.ENUM('member', 'moderator', 'admin'), defaultValue: 'member' },
-  joinedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-  isMuted: { type: DataTypes.BOOLEAN, defaultValue: false },
-  nickname: { type: DataTypes.STRING, defaultValue: null },
-});
+  role: { type: DataTypes.STRING, defaultValue: 'member' },
+}, { timestamps: true });
