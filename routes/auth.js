451bfca709071fff8d7a964317a69c83260ca0bb
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 13:38:36 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth');
-const { register, login, logout, getMe, easyLogin } = require('../controllers/authController');
+const { register, login, logout, getMe } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', auth, logout);
 router.get('/me', auth, getMe);
-router.post('/easy-login', easyLogin);
 
 module.exports = router;
