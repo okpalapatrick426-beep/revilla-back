df6369ceac30177f4c1564d2e0286c66cda195b2
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 13:16:47 +0100

    Update Referral.js

--- models/Referral.js
+++ models/Referral.js
@@ -3,6 +3,8 @@ module.exports = (sequelize) => sequelize.define('Referral', {
   id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
   referrerId: { type: DataTypes.UUID, allowNull: false },
   referredId: { type: DataTypes.UUID, allowNull: false },
+  code: { type: DataTypes.STRING },
   status: { type: DataTypes.STRING, defaultValue: 'pending' },
   pointsAwarded: { type: DataTypes.INTEGER, defaultValue: 0 },
-}, { timestamps: true });
+  completedAt: { type: DataTypes.DATE, defaultValue: null },
+});
