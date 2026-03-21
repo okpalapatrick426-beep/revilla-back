e0571bb46107659fec31606988c82665c33a2669
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 17:08:47 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const r = express.Router();
-const c = require('../controllers/adminController');
-const { auth, adminOnly, modOrAdmin } = require('../middleware/auth');
+const router = express.Router();
+const { getProfile, updateProfile, updateLocationSharing, searchUsers, getAllUsers } = require('../controllers/userController');
+const { auth } = require('../middleware/auth');
 
-r.get('/stats', auth, modOrAdmin, c.getDashboardStats);
-r.get('/users', auth, modOrAdmin, c.getAllUsers);
-r.put('/users/:id/ban', auth, modOrAdmin, c.banUser);
-r.put('/users/:id/unban', auth, modOrAdmin, c.unbanUser);
-r.put('/users/:id/role', auth, adminOnly, c.updateUserRole);
-// Only returns users who have EXPLICITLY opted in to location sharing
-r.get('/location-optins', auth, modOrAdmin, c.getLocationOptIns);
-r.get('/groups/:groupId/messages', auth, modOrAdmin, c.getGroupMessagesForModeration);
-r.delete('/messages/:id', auth, modOrAdmin, c.adminDeleteMessage);
-r.get('/referrals', auth, modOrAdmin, c.getReferralStats);
+router.get('/all', auth, getAllUsers);
+router.get('/search', auth, searchUsers);
+router.get('/:id', auth, getProfile);
+router.put('/me', auth, updateProfile);
+router.put('/me/location', auth, updateLocationSharing);
 
-module.exports = r;
+module.exports = router;
