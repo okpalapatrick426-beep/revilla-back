f3dff930a356c7b0c819a5841b4c346114dee4e8
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 16:53:31 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
+// routes/auth.js
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth');
 const { register, login, logout, getMe } = require('../controllers/authController');
+const { auth } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
