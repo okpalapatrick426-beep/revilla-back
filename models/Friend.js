befe746968b83379c55ff6058b94d0447b63b5b5
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 13:15:00 +0100

    Update Friend.js

--- models/Friend.js
+++ models/Friend.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => sequelize.define('Friend', {
   id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
-  requesterId: { type: DataTypes.UUID, allowNull: false },
-  receiverId: { type: DataTypes.UUID, allowNull: false },
+  userId: { type: DataTypes.UUID, allowNull: false },
+  friendId: { type: DataTypes.UUID, allowNull: false },
   status: { type: DataTypes.STRING, defaultValue: 'pending' },
-}, { timestamps: true });
+});
