e71e55ce724811e1e0333761ae47eedf38990df4
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 18:08:33 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
+// routes/users.js
 const express = require('express');
 const router = express.Router();
 const { getProfile, updateProfile, updateLocationSharing, searchUsers, getAllUsers } = require('../controllers/userController');
 const { auth } = require('../middleware/auth');
 
+// IMPORTANT: /me and /search must come BEFORE /:id
+// otherwise Express matches "me" and "search" as an :id param
 router.get('/all', auth, getAllUsers);
 router.get('/search', auth, searchUsers);
-router.get('/:id', auth, getProfile);
 router.put('/me', auth, updateProfile);
 router.put('/me/location', auth, updateLocationSharing);
+router.get('/:id', auth, getProfile);
 
 module.exports = router;
