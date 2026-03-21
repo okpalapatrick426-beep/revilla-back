58fdae2171737e5e830f51729ed66b95354e7311
okpalapatrick426-beep <okpalapatrick426@gmail.com>
2026-03-21 13:15:42 +0100

    Update GroupMember.js

--- models/GroupMember.js
+++ models/GroupMember.js
@@ -4,4 +4,4 @@ module.exports = (sequelize) => sequelize.define('GroupMember', {
   groupId: { type: DataTypes.UUID, allowNull: false },
   userId: { type: DataTypes.UUID, allowNull: false },
   role: { type: DataTypes.STRING, defaultValue: 'member' },
-}, { timestamps: true });
+});
